a78f5c821912111c7c8926e39a004ec89ada0661
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-06-13 18:12:39 -0300

    fix:task/home: Removed unnecessary icon and text elements and Changed text color for alerts from base-300 to white

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -19,17 +19,13 @@ function Alert({ alerts = [] }: Props) {
     <div class="bg-secondary">
       <div class="flex items-center gap-12 container">
         <div class="flex items-center justify-between gap-1">
-          <Icon id="pin" size={20} strokeWidth={16} />
-          <span class="text-[13px] text-base-300 flex justify-center items-center h-[38px]">
-            22471-003
-          </span>
         </div>
         <span class="hidden sm:block w-[1px] h-[25px] bg-base-300">
         </span>
         {alerts.map((alert) => (
           <a
             href={alert.link}
-            class="hidden sm:flex text-[13px] text-base-300 justify-center items-center h-[38px]"
+            class="hidden sm:flex text-[13px] text-white justify-center items-center h-[38px]"
           >
             {alert.label}
           </a>
