88d0ffb667c0cdaf2d012f31f71a3c05781dce17
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-06-13 18:15:33 -0300

    feat:task/home -  -Added isShow prop for conditional rendering of children - Updated styles and layout

--- components/header/NavItem.tsx
+++ components/header/NavItem.tsx
@@ -1,24 +1,30 @@
 import type { SiteNavigationElement } from "apps/commerce/types.ts";
 import Image from "apps/website/components/Image.tsx";
 import { HEADER_HEIGHT } from "../../constants.ts";
+import { useSection } from "deco/hooks/useSection.ts";
 
-function NavItem({ item }: { item: SiteNavigationElement }) {
+function NavItem(
+  { item, isShow = false }: { item: SiteNavigationElement; isShow: boolean },
+) {
   const { url, name, children } = item;
   const image = item?.image?.[0];
 
   return (
     <li class="group flex items-center">
-      <a href={url} class="py-6">
-        <span class="group-hover:underline text-xs font-thin">
+      <a
+        href={url}
+        class="py-6"
+      >
+        <span class="group-hover:underline uppercase font-roboto text-15 font-normal text-lightGray  ">
           {name}
         </span>
       </a>
 
-      {children && children.length > 0 &&
+      {children && children.length > 0 && isShow &&
         (
           <div
             class="fixed hidden hover:flex group-hover:flex bg-base-100 z-40 items-start justify-center gap-6 border-t border-b-2 border-base-200 w-screen"
-            style={{ top: "0px", left: "0px", marginTop: HEADER_HEIGHT }}
+            style={{ top: "0px", left: "0px", marginTop: "150px" }}
           >
             {image?.url && (
               <Image
