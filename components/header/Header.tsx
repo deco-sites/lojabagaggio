82a40b57af8be8d24ff1407a9bf72d9a6c98dae5
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-06-14 17:18:40 -0300

    feat:task/header -  Removed SEARCHBAR_POPUP_ID. Updated Desktop component: Removed the modal for the search bar. Adjusted the logo position and structure. Moved the search bar into a more central position. Adjusted the layout of navigation items and additional elements such as the wishlist and login.

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -7,14 +7,13 @@ import {
   HEADER_HEIGHT,
   NAVBAR_HEIGHT,
   SEARCHBAR_DRAWER_ID,
-  SEARCHBAR_POPUP_ID,
   SIDEMENU_CONTAINER_ID,
   SIDEMENU_DRAWER_ID,
 } from "../../constants.ts";
 import Searchbar, { type SearchbarProps } from "../search/Searchbar/Form.tsx";
 import Drawer from "../ui/Drawer.tsx";
 import Icon from "../ui/Icon.tsx";
-import Modal from "../ui/Modal.tsx";
+// import Modal from "../ui/Modal.tsx"; // Removido pois não está em uso
 import Alert from "./Alert.tsx";
 import Bag from "./Bag.tsx";
 import Login from "./Login.tsx";
@@ -55,55 +54,45 @@ const Desktop = (
   { navItems, logo, searchbar }: Props,
 ) => (
   <>
-    <Modal id={SEARCHBAR_POPUP_ID}>
-      <div
-        class="absolute top-0 bg-base-100 container"
-        style={{ marginTop: HEADER_HEIGHT }}
-      >
-        <Searchbar {...searchbar} />
-      </div>
-    </Modal>
-
-    <div class="grid grid-cols-3 items-center border-b border-base-200 w-full px-6">
-      <ul class="flex gap-6 col-span-1 justify-start">
-        {navItems?.slice(0, 4).map((item) => <NavItem item={item} />)}
-      </ul>
-      <div class="flex justify-center">
-        {logo && (
-          <a href="/" aria-label="Store logo">
-            <Image
-              src={logo.src}
-              alt={logo.alt}
-              width={logo.width || 100}
-              height={logo.height || 23}
-            />
-          </a>
-        )}
-      </div>
-      <div class="flex-none flex items-center justify-end gap-2 col-span-1">
-        <label
-          for={SEARCHBAR_POPUP_ID}
-          class="btn btn-sm btn-ghost font-thin no-animation"
-          aria-label="search icon button"
-        >
-          <Icon id="MagnifyingGlass" size={20} strokeWidth={0.1} />
-          <span>SEARCH</span>
-        </label>
-
-        <Login />
-
-        <a
-          class="btn btn-sm btn-ghost font-thin no-animation"
-          href="/wishlist"
-          aria-label="Wishlist"
-        >
-          <Icon id="Heart" size={24} strokeWidth={0.4} />
-          <span>WISHLIST</span>
-        </a>
-
-        <div class="flex items-center text-xs font-thin">
-          <Bag />
+    <div className="flex flex-col w-full px-6 items-center">
+      <div className="w-full flex justify-between items-center py-3">
+        <div className="flex justify-between items-center py-3">
+          {logo && (
+            <a href="/" aria-label="Store logo">
+              <Image
+                src={logo.src}
+                alt={logo.alt}
+                width={logo.width || 100}
+                height={logo.height || 23}
+              />
+            </a>
+          )}
         </div>
+
+        <div className="flex items-center w-full">
+          <Searchbar {...searchbar} />
+        </div>
+
+        <div className="flex-none flex items-center justify-end gap-2 col-span-1">
+          <Login />
+          <a
+            className="btn btn-sm btn-ghost font-thin no-animation"
+            href="/wishlist"
+            aria-label="Wishlist"
+          >
+            <Icon id="Heart" size={24} strokeWidth={0.4} />
+          </a>
+
+          <div className="flex items-center text-xs font-thin">
+            <Bag />
+          </div>
+        </div>
+      </div>
+
+      <div className="lex w-full">
+        <ul className="flex gap-6 col-span-1 justify-start">
+          {navItems?.slice(0, 4).map((item) => <NavItem key={item.url} item={item} />)}
+        </ul>
       </div>
     </div>
   </>
@@ -115,7 +104,7 @@ const Mobile = ({ logo, searchbar }: Props) => (
       id={SEARCHBAR_DRAWER_ID}
       aside={
         <Drawer.Aside title="Search" drawer={SEARCHBAR_DRAWER_ID}>
-          <div class="w-screen overflow-y-auto">
+          <div className="w-screen overflow-y-auto">
             <Searchbar {...searchbar} />
           </div>
         </Drawer.Aside>
@@ -127,10 +116,10 @@ const Mobile = ({ logo, searchbar }: Props) => (
         <Drawer.Aside title="Menu" drawer={SIDEMENU_DRAWER_ID}>
           <div
             id={SIDEMENU_CONTAINER_ID}
-            class="h-full flex items-center justify-center"
+            className="h-full flex items-center justify-center"
             style={{ minWidth: "100vw" }}
           >
-            <span class="loading loading-spinner" />
+            <span className="loading loading-spinner" />
           </div>
         </Drawer.Aside>
       }
@@ -138,11 +127,11 @@ const Mobile = ({ logo, searchbar }: Props) => (
 
     <div
       style={{ height: NAVBAR_HEIGHT }}
-      class="grid grid-cols-3 justify-between items-center border-b border-base-200 w-full px-6 pb-6 gap-2"
+      className="grid grid-cols-3 justify-between items-center border-b border-base-200 w-full px-6 pb-6 gap-2"
     >
       <label
-        for={SIDEMENU_DRAWER_ID}
-        class="btn btn-circle md:btn-sm btn-xs btn-ghost"
+        htmlFor={SIDEMENU_DRAWER_ID}
+        className="btn btn-circle md:btn-sm btn-xs btn-ghost"
         aria-label="open menu"
         hx-target={`#${SIDEMENU_CONTAINER_ID}`}
         hx-swap="outerHTML"
@@ -154,7 +143,7 @@ const Mobile = ({ logo, searchbar }: Props) => (
       {logo && (
         <a
           href="/"
-          class="flex-grow inline-flex items-center justify-center"
+          className="flex-grow inline-flex items-center justify-center"
           style={{ minHeight: NAVBAR_HEIGHT }}
           aria-label="Store logo"
         >
@@ -167,10 +156,10 @@ const Mobile = ({ logo, searchbar }: Props) => (
         </a>
       )}
 
-      <div class="flex justify-end gap-1">
+      <div className="flex justify-end gap-1">
         <label
-          for={SEARCHBAR_DRAWER_ID}
-          class="btn btn-circle btn-sm btn-ghost"
+          htmlFor={SEARCHBAR_DRAWER_ID}
+          className="btn btn-circle btn-sm btn-ghost"
           aria-label="search icon button"
         >
           <Icon id="MagnifyingGlass" size={20} strokeWidth={0.1} />
@@ -205,7 +194,7 @@ function Header({
       hx-target="closest section"
       hx-swap="outerHTML"
     >
-      <div class="bg-base-100 fixed w-full z-40">
+      <div className="bg-base-100 fixed w-full z-40">
         {alerts.length > 0 && <Alert alerts={alerts} />}
         {device === "desktop"
           ? <Desktop logo={logo} {...props} />
