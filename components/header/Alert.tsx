6cf0e7ac1503af355b770e921825375d49e5d7ea
Lucas Leite <70826073+leitecsleite@users.noreply.github.com>
2024-06-14 11:47:13 -0300

    test

--- components/header/Alert.tsx
+++ components/header/Alert.tsx
@@ -20,6 +20,7 @@ function Alert({ alerts = [], interval = 5 }: Props) {
           <Slider.Item index={index} class="carousel-item">
             <span class="text-sm text-secondary-content flex justify-center items-center w-screen h-[38px]">
               {alert}
+              
             </span>
           </Slider.Item>
         ))}
