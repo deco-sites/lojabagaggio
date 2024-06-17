18726f0b826350f5d7a6cbd4aed30a2ab815f848
Lucas Leite <lucas.leite@agencian1.com.br>
2024-06-17 22:00:00 +0000

    Release staging environment with 0 changes made by

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -6,6 +6,7 @@ import { useSection } from "deco/hooks/useSection.ts";
 import {
   HEADER_HEIGHT,
   NAVBAR_HEIGHT,
+  SEARCHBAR_DRAWER_ID,
   SIDEMENU_CONTAINER_ID,
   SIDEMENU_DRAWER_ID,
 } from "../../constants.ts";
@@ -18,7 +19,6 @@ import Bag from "./Bag.tsx";
 import Login from "./Login.tsx";
 import Menu from "./Menu.tsx";
 import NavItem from "./NavItem.tsx";
-import { AppContext } from "../../apps/site.ts";
 
 export interface Logo {
   src: ImageWidget;
@@ -54,17 +54,16 @@ const Desktop = (
   { navItems, logo, searchbar }: Props,
 ) => (
   <>
-    <div className="flex flex-col w-9/10 m-auto max-w-screen-3xl items-center">
-      <div className="w-full flex justify-between items-center py-[0.7rem]">
-        <div className="flex justify-between items-center py-3 w-full max-w-45">
+    <div className="flex flex-col w-full px-6 items-center">
+      <div className="w-full flex justify-between items-center py-3">
+        <div className="flex justify-between items-center py-3">
           {logo && (
-            <a href="/" aria-label="Store logo" class="w-full">
+            <a href="/" aria-label="Store logo">
               <Image
                 src={logo.src}
                 alt={logo.alt}
                 width={logo.width || 100}
                 height={logo.height || 23}
-                class="w-full max-w-45"
               />
             </a>
           )}
@@ -74,14 +73,14 @@ const Desktop = (
           <Searchbar {...searchbar} />
         </div>
 
-        <div className="flex-none flex items-center justify-end gap-6">
+        <div className="flex-none flex items-center justify-end gap-2 col-span-1">
           <Login />
           <a
-            className=" no-animation"
+            className="btn btn-sm btn-ghost font-thin no-animation"
             href="/wishlist"
             aria-label="Wishlist"
           >
-            <Icon id="wishlist" size={28} strokeWidth={0.4} />
+            <Icon id="Heart" size={28} strokeWidth={0.4} />
           </a>
 
           <div className="flex items-center text-xs font-thin">
@@ -91,16 +90,26 @@ const Desktop = (
       </div>
 
       <div className="lex w-full">
-        <ul className="flex gap-4 items-center justify-between px-4">
-          {navItems?.map((item) => <NavItem key={item.url} item={item} />)}
+        <ul className="flex gap-6 col-span-1 justify-start">
+          {navItems?.slice(0, 4).map((item) => <NavItem key={item.url} item={item} />)}
         </ul>
       </div>
     </div>
   </>
 );
 
-const Mobile = ({ logo, device, searchbar }: Props & { device?: string }) => (
+const Mobile = ({ logo, searchbar }: Props) => (
   <>
+    <Drawer
+      id={SEARCHBAR_DRAWER_ID}
+      aside={
+        <Drawer.Aside title="Search" drawer={SEARCHBAR_DRAWER_ID}>
+          <div className="w-screen overflow-y-auto">
+            <Searchbar {...searchbar} />
+          </div>
+        </Drawer.Aside>
+      }
+    />
     <Drawer
       id={SIDEMENU_DRAWER_ID}
       aside={
@@ -115,49 +124,47 @@ const Mobile = ({ logo, device, searchbar }: Props & { device?: string }) => (
         </Drawer.Aside>
       }
     />
+
     <div
-      class="flex flex-col w-full  px-6 pb-6 gap-2"
       style={{ height: NAVBAR_HEIGHT }}
+      className="grid grid-cols-3 justify-between items-center border-b border-base-200 w-full px-6 pb-6 gap-2"
     >
-      <div className="grid grid-cols-2 justify-between items-center w-full">
-        <div class="flex items-center justify-around">
-          <label
-            htmlFor={SIDEMENU_DRAWER_ID}
-            className="btn btn-circle md:btn-sm btn-xs btn-ghost mr-6"
-            aria-label="open menu"
-            hx-target={`#${SIDEMENU_CONTAINER_ID}`}
-            hx-swap="outerHTML"
-            hx-trigger="click once"
-            hx-get={useSection({ props: { variant: "menu" } })}
-          >
-            <Icon id="Bars3" size={20} strokeWidth={0.01} />
-          </label>
-          {logo && (
-            <a
-              href="/"
-              className="flex-grow inline-flex items-center justify-center"
-              style={{ minHeight: NAVBAR_HEIGHT }}
-              aria-label="Store logo"
-            >
-              <Image
-                src={logo.src}
-                alt={logo.alt}
-                width={logo.width || 100}
-                height={logo.height || 13}
-                class=" w-full max-w-32"
-              />
-            </a>
-          )}
-        </div>
+      <label
+        htmlFor={SIDEMENU_DRAWER_ID}
+        className="btn btn-circle md:btn-sm btn-xs btn-ghost"
+        aria-label="open menu"
+        hx-target={`#${SIDEMENU_CONTAINER_ID}`}
+        hx-swap="outerHTML"
+        hx-trigger="click once"
+        hx-get={useSection({ props: { variant: "menu" } })}
+      >
+        <Icon id="Bars3" size={20} strokeWidth={0.01} />
+      </label>
+      {logo && (
+        <a
+          href="/"
+          className="flex-grow inline-flex items-center justify-center"
+          style={{ minHeight: NAVBAR_HEIGHT }}
+          aria-label="Store logo"
+        >
+          <Image
+            src={logo.src}
+            alt={logo.alt}
+            width={logo.width || 100}
+            height={logo.height || 13}
+          />
+        </a>
+      )}
 
-        <div className="flex justify-end gap-1">
-          <Bag device={device} />
-        </div>
-      </div>
-
-      {/** search */}
-      <div className=" w-full">
-        <Searchbar {...searchbar} device={device} />
+      <div className="flex justify-end gap-1">
+        <label
+          htmlFor={SEARCHBAR_DRAWER_ID}
+          className="btn btn-circle btn-sm btn-ghost"
+          aria-label="search icon button"
+        >
+          <Icon id="MagnifyingGlass" size={20} strokeWidth={0.1} />
+        </label>
+        <Bag />
       </div>
     </div>
   </>
@@ -173,7 +180,7 @@ function Header({
     alt: "Logo",
   },
   ...props
-}: Props & { device?: string }) {
+}: Props) {
   const device = useDevice();
 
   return (
@@ -191,22 +198,16 @@ function Header({
         {alerts.length > 0 && <Alert alerts={alerts} />}
         {device === "desktop"
           ? <Desktop logo={logo} {...props} />
-          : <Mobile logo={logo} {...props} device={device} />}
+          : <Mobile logo={logo} {...props} />}
       </div>
     </header>
   );
 }
 
-export default function Section(
-  { variant, device, ...props }: SectionProps & { device?: string },
-) {
+export default function Section({ variant, ...props }: SectionProps) {
   if (variant === "menu") {
     return <Menu navItems={props.navItems ?? []} />;
   }
 
-  return <Header {...props} device={device} />;
+  return <Header {...props} />;
 }
-
-export const loader = (props: Props, _req: Request, ctx: AppContext) => {
-  return { ...props, device: ctx.device };
-};
